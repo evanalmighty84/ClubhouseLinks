d8db11ab99df48953f8ebb46da13fc95b679656f
evanalmighty84 <Godlovesme24!!>
2025-05-21 08:01:34 -0500

    added db path

--- functions/hotlist_cron/index.js
+++ functions/hotlist_cron/index.js
@@ -1,5 +1,5 @@
 const moment = require('moment');
-const pool = require('../../db/db'); // adjust path if needed
+const pool = require('./db/db'); // adjust path if needed
 require('dotenv').config();
 
 (async () => {
