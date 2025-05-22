8b115e673ab59e7ed5d2cb374dd2917c93560b6f
evanalmighty84 <Godlovesme24!!>
2025-05-22 16:05:59 -0500

    Update server path after flattening crm_function

--- functions/server.js
+++ functions/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const crmApp = require('./crm_function'); // crm_function/index.js should export an app or router
+const crmApp = require('./index'); // crm_function/index.js should export an app or router
 
 app.use('/server/crm_function/', crmApp);
 
