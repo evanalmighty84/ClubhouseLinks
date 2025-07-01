85567efb50eb8b9a43840858734366cb2ecb888d
evanalmighty84 <Godlovesme24!!>
2025-06-30 22:01:33 -0500

    deleted extra run

--- functions/ionwave_cron/run.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const runIonWaveAutomation = require('./index');
-
-runIonWaveAutomation()
-    .then(() => console.log('IonWave automation completed.'))
-    .catch((err) => {
-        console.error('Error running IonWave automation:', err);
-        process.exit(1);
-    });
