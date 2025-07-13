20f71cd040d00a64453c9acf1cc20e658f02a8ef
evanalmighty84 <Godlovesme24!!>
2025-07-12 20:40:26 -0500

    you know

--- functions/routes/emailQueuedRoutes.js
+++ functions/routes/emailQueuedRoutes.js
@@ -5,7 +5,7 @@ const emailQueuedController = require('../controllers/emailQueuedController');
 // get Email Queued for user
 router.post('/showEmails', emailQueuedController.getEmailQueued);
 router.post('/pendingEmails', emailQueuedController.getPendingEmailQueued);
-router.post('/campaignsandtemplates/all', emailQueuedController.getAllEmails);
+router.post('/campaignsandtemplates', emailQueuedController.getAllEmails);
 
 
 
