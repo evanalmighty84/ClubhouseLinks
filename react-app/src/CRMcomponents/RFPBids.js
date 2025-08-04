6ed38645ac5df09b6a37ca888dfe87fb694c7f5b
evanalmighty84 <Godlovesme24!!>
2025-08-04 14:14:56 -0500

    added railway endpoint

--- react-app/src/CRMcomponents/RFPBids.js
+++ react-app/src/CRMcomponents/RFPBids.js
@@ -20,7 +20,7 @@ const RFPBids = () => {
     useEffect(() => {
         const fetchBids = async () => {
             try {
-                const res = await fetch('https://crm-function-app-5d4de511071d.herokuapp.com/server/crm_function/api/ionwave/bids'); // ✅ LOCAL
+                const res = await fetch('https://upbeat-spontaneity-production.up.railway.app/server/crm_function/api/ionwave/bids'); // ✅ LOCAL
                 const data = await res.json();
 
                 if (data.success && data.bids.length > 0) {
