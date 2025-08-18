6f7db091a44f2cb0b0d65b71a9e10127c7ba0395
evanalmighty84 <Godlovesme24!!>
2025-08-18 11:58:35 -0500

    added rfp to leads page

--- react-app/src/CRMpages/LeadsPage.js
+++ react-app/src/CRMpages/LeadsPage.js
@@ -4,8 +4,10 @@ import NextDoorLeads from "../CRMcomponents/NextDoorLeads";
 
 const LeadsPage = () => {
     return (
+        <>
        <RFPBids/>,
         <NextDoorLeads/>
+            </>
     );
 };
 
