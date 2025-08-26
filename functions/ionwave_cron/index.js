a5b04d31bdb72d3ab9431814bd93a18cd0e4d62c
evanalmighty84 <Godlovesme24!!>
2025-08-26 12:44:14 -0500

    added wylie back

--- functions/ionwave_cron/index.js
+++ functions/ionwave_cron/index.js
@@ -12,11 +12,11 @@ const { runMelissaAutomation }= require('./utils/melissaLookup');
 /*        console.log('\n📩 Running IonWave Automation...');
         await runIonWaveAutomation();*/
 
-      console.log('\n🏘️ Running Nextdoor Automation...');
-        await runAutomation();
+  /*    console.log('\n🏘️ Running Nextdoor Automation...');
+        await runAutomation();*/
 
-/*        console.log('running Melissa Automation')
-        await runMelissaAutomation();*/
+       console.log('running Melissa Automation')
+        await runMelissaAutomation();
 
         console.log('\n✅ All automations completed');
     } catch (err) {
