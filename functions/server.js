9f9c8bcb4ae1838e6f1e93ae01cf5e49a67e2dc3
evanalmighty84 <evanligon7@gmail.com>
2025-11-14 12:58:51 -0600

    twilio encoding

--- functions/server.js
+++ functions/server.js
@@ -1,5 +1,9 @@
+
 const express = require('express');
 const app = express();
+// ⭐ ADD THESE TWO LINES BEFORE ANY ROUTES ⭐
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const crmApp = require('./crmIndex'); // crm_function/crmIndex.js should export an app or router
 const leadApp = require('./leadindex'); // crm_function/crmIndex.js should export an app or router
 app.use('/server/crm_function/', crmApp);
